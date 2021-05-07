09aafdc527b27dfcbfee7806ca8399adc9a46c2c
Xurmusha <juraboeva98@inbox.ru>
2021-05-07 16:53:57 +0500

    Project Completed

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function clickButton(){
+    document.getElementById('btn-large').classList.toggle('active');
+}
+function clickButtonTwo(){
+    document.getElementById('btn-medium').classList.toggle('active');
+}
+function clickButtonThree(){
+    document.getElementById('btn-small').classList.toggle('active');
+}
+function closeField() {
+    document.getElementById('doubleRoom').classList.toggle('d-none')
+}
